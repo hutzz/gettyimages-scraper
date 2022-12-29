3109be5b269f8c0bfe80d4a80319e66de4978488
Zach <zachhutz0@gmail.com>
2022-12-29 12:57:57 -0500

    added directory creation

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import urllib.request
+import os
 
 NUM_PAGES = int(input("Enter number of pages to scrape: "))
 URL = input("Enter URL of GettyImages search: ")
+
+if not os.path.exists('images'):
+    os.makedirs('images')
+
 driver = webdriver.Chrome()
 i = 0
 for x in range(1, NUM_PAGES+1):
     page = f"{URL}&page={x}"
     driver.get(page)
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(4)
     print(f"page {x}")
     elements = driver.find_elements(By.CLASS_NAME, "MosaicAsset-module__thumb___yvFP5")
     for f, e in enumerate(elements):
